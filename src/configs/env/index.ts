338d7ef9f31849270a7e8eb6dda71120a59ff264
Besmillah Ibrahimi <besmillah@ibrahimi.info>
2025-05-22 09:09:21 +0330

    setup env

--- src/configs/env/index.ts
+++ src/configs/env/index.ts
@@ -2,20 +2,16 @@ import { envSchema } from "./schema";
 
 export const Env = envSchema.parse({
   supabase: {
-    url: "https://lujrcqmwmpvxymyrphbm.supabase.co",
-    anonKey:
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx1anJjcW13bXB2eHlteXJwaGJtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY2OTkwMTIsImV4cCI6MjA2MjI3NTAxMn0.oJVLn4eEQk1WnADAzQ3UNVwPsOe47FWXCpZu4QzpGiM",
+    url: process.env.EXPO_PUBLIC_SUPABASE_URL,
+    anonKey: process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY,
   },
   app: {
-    name: "Educational App",
-    schema: "com.besmillahibrahimi.educationalapp",
+    name: process.env.EXPO_PUBLIC_APP_NAME,
+    schema: process.env.EXPO_PUBLIC_APP_SCHEMA,
   },
   google: {
-    androidClientId:
-      "1043752002269-u0g66sde8ucj542pk5vs4c7035m3r7sh.apps.googleusercontent.com", // android
-    webClientId:
-      "1043752002269-a9t1atmqng5lq63m5f1en5d312gaaa6a.apps.googleusercontent.com", // web
-    iosClientId:
-      "1043752002269-g2qkatqj11dhdb6dja93d7f4qki4tmau.apps.googleusercontent.com", // ios
+    androidClientId: process.env.EXPO_PUBLIC_GOOGLE_ANDROID_CLIENT_ID,
+    webClientId: process.env.EXPO_PUBLIC_GOOGLE_WEB_CLIENT_ID,
+    iosClientId: process.env.EXPO_PUBLIC_GOOGLE_IOS_CLIENT_ID,
   },
 });
